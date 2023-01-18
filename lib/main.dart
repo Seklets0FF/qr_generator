614c534e807d4e47d9a54c54b2ea02ab43d4a682
sseklecov <s.sekletsov@gmail.com>
2023-01-18 17:47:27 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Визитка'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(widget.title, style: const TextStyle(color: Colors.black)),
+      ),
+      body: Center(
+        child: QrImage(
+          data: 'https://www.youtube.com/@sekletsoff?sub_confirmation=1',
+          version: QrVersions.auto,
+          size: 300.00,
+          semanticsLabel: 'Лучший влог программиста! :)',
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.red,
+          gapless: false,
+        ),
+      ),
+      backgroundColor: Colors.blueGrey,
+    );
+  }
+}
